f948cf6ad10544827d996cd2fc4d549c190d4f56
Akshathello <akshatkhero@gmail.com>
2024-11-15 11:30:37 +0530

    ResponsiveFixTocards

--- src/pages/Home/WhyUs.jsx
+++ src/pages/Home/WhyUs.jsx
@@ -20,15 +20,26 @@ function WhyUs() {
   // }
 
   return (
+    <>    
+    <div className = "h-64 bg-black">
+    <div      
+       className= " lg:hidden flex flex-col lg:w-2/4 w-full lg:justify-center lg:items-start items-center lg:pl-14 p-4 h-[43rem] ">
+      <div className ="flex justify-start items-center font-archivo text-[#FECE02] text-[80px] font-medium whitespace-nowrap">WHY US?</div>
+      <div className= "flex font-inter font-normal text-[20px] text-white pb-36 lg:text-start text-center">Lorem Ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</div>
+      </div>
+      </div>
+
+
     <div className = "bg-black min-h-screen">
 
-      <div className ="flex flex-row">
-        <div       style = {{
+      <div className ="flex lg:flex-row flex-col">
+        <div       
+        style = {{
         position: 'sticky',
-        top: '20px', 
-      }} className= "flex flex-col w-2/4 justify-center pl-14 h-[43rem]">
-        <div className ="flex justify-start items-center font-archivo text-[#FECE02] text-[80px] font-medium">WHY US?</div>
-        <div className= "flex font-inter font-normal text-[20px] text-white pb-36">Lorem Ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</div>
+        top: '190px', 
+      }} className= " lg:block hidden flex flex-col lg:w-2/4 w-full lg:justify-center lg:items-start items-center lg:pl-14 p-4 h-[43rem] ">
+        <div className ="flex justify-start items-center font-archivo text-[#FECE02] text-[80px] font-medium whitespace-nowrap">WHY US?</div>
+        <div className= "flex font-inter font-normal text-[20px] text-white pb-36 lg:text-start text-center">Lorem Ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</div>
         </div>
 
         <div className = "flex flex-col w-full justify-around items-center gap-32 h-[256rem] " 
@@ -44,6 +55,8 @@ function WhyUs() {
 
       </div>
     </div>
+    </>
+
   )
 }
 
