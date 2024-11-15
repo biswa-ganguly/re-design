325ba41ff6d0d0ae00841e5d7b398d9081774088
Akshathello <akshatkhero@gmail.com>
2024-11-15 19:00:22 +0530

    fix

--- src/pages/Home/WhyUs.jsx
+++ src/pages/Home/WhyUs.jsx
@@ -22,8 +22,12 @@ function WhyUs() {
   return (
     <>    
     <div className = "h-64 bg-black">
-    <div      
-       className= " lg:hidden flex flex-col lg:w-2/4 w-full lg:justify-center lg:items-start items-center lg:pl-14 p-4 h-[43rem] ">
+    <div  
+       style={{
+        postion:'sticky',
+        top:'50px'
+       }}    
+       className= " lg:hidden flex flex-col lg:w-2/4 w-full lg:justify-center lg:items-start items-center lg:pl-14 p-4">
       <div className ="flex justify-start items-center font-archivo text-[#FECE02] text-[80px] font-medium whitespace-nowrap">WHY US?</div>
       <div className= "flex font-inter font-normal text-[20px] text-white pb-36 lg:text-start text-center">Lorem Ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</div>
       </div>
@@ -37,12 +41,12 @@ function WhyUs() {
         style = {{
         position: 'sticky',
         top: '190px', 
-      }} className= " lg:block hidden flex flex-col lg:w-2/4 w-full lg:justify-center lg:items-start items-center lg:pl-14 p-4 h-[43rem] ">
+      }} className= " lg:block hidden flex flex-col lg:w-2/4 w-full lg:justify-center lg:items-start items-center lg:pl-14 p-4 lg:h-[43rem] md:h-[0rem] h-[0rem] ">
         <div className ="flex justify-start items-center font-archivo text-[#FECE02] text-[80px] font-medium whitespace-nowrap">WHY US?</div>
         <div className= "flex font-inter font-normal text-[20px] text-white pb-36 lg:text-start text-center">Lorem Ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</div>
         </div>
 
-        <div className = "flex flex-col w-full justify-around items-center gap-32 h-[256rem] " 
+        <div className = "flex flex-col w-full justify-around items-center gap-32 lg:h-[256rem] md:h-[130rem] h-[120rem]  " 
       >
 
        <CardForWhyUS/>
