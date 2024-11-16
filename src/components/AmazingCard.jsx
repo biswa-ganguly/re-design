820c59416b63beb133fbac8eacf8186bc4830519
Akshathello <akshatkhero@gmail.com>
2024-11-16 19:55:27 +0530

    AmazingCardFix

--- src/components/AmazingCard.jsx
+++ src/components/AmazingCard.jsx
@@ -1,15 +1,15 @@
 const AmazingCard = ({ title, description, tags }) => {
   return (
-    <div className="w-[550px] h-[500px] lg:w-[620px] lg:h-[560px] rounded-[32px] border-[3px] border-[#6735EA] flex flex-col gap-5 py-4 px-4 items-center">
-      <div className="w-[520px] h-[270px] lg:w-[588px] lg:h-[320px] bg-[#FECE02] rounded-[16px] transform transition-transform duration-500 hover:scale-95 hover:rotate-2 hover:translate-x-1 hover:translate-y-1"></div>
+    <div className="md:w-[550px] md:h-[500px] lg:w-[620px] lg:h-[560px] w-[380px] h-[500px] rounded-[32px] border-[3px] border-[#6735EA] flex flex-col gap-5 py-4 px-4 items-center">
+      <div className="md:w-[520px] md:h-[270px] lg:w-[588px] lg:h-[320px] w-[350px] h-[500px] bg-[#FECE02] rounded-[16px] transform transition-transform duration-500 hover:scale-95 hover:rotate-2 hover:translate-x-1 hover:translate-y-1"></div>
 
       <div className="flex gap-3 w-full">
-        {tags?.map((arra, index) => (
+        {tags?.map((array, index) => (
           <p
             key={index}
             className="w-fit rounded-[16px] bg-[#FECE02] py-[6px] px-[12px] font-medium"
           >
-            {arra}
+            {array}
           </p>
         ))}
       </div>
