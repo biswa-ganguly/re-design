f6701c47e829fd8430eb015288267a0e6f616262
farhan-alam18 <farhanalam1113@gmail.com>
2024-11-29 02:01:57 +0530

    Mockups Added

--- src/components/AmazingCard.jsx
+++ src/components/AmazingCard.jsx
@@ -1,7 +1,17 @@
-const AmazingCard = ({ title, description, tags }) => {
+const AmazingCard = ({ title, description, tags, image }) => {
   return (
-    <div className="md:w-[550px] md:h-[500px] lg:w-[620px] lg:h-[560px] w-[380px] h-[500px] rounded-[32px] border-[3px] border-[#6735EA] flex flex-col gap-5 py-4 px-4 items-center">
-      <div className="md:w-[520px] md:h-[270px] lg:w-[588px] lg:h-[320px] w-[350px] h-[500px] bg-[#FECE02] rounded-[16px] transform transition-transform duration-500 hover:scale-95 hover:rotate-2 hover:translate-x-1 hover:translate-y-1"></div>
+    <div className="md:w-[550px] md:h-[500px] lg:w-[620px] lg:h-[560px] w-[380px] h-[500px] rounded-[32px] border-[4px] border-[#6735EA] flex flex-col gap-5 py-4 px-4 items-center">
+      <div className="rounded-[16px] transform transition-transform duration-500 hover:scale-95 hover:rotate-2 hover:translate-x-1 hover:translate-y-1">
+        {image ? (
+          <img
+            src={image}
+            alt={title}
+            className="md:w-[520px] md:h-[270px] lg:w-[588px] lg:h-[320px] w-[350px] h-[500px] rounded-[16px]"
+          />
+        ) : (
+          <div className="md:w-[520px] md:h-[270px] lg:w-[588px] lg:h-[320px] w-[350px] h-[500px] rounded-[16px]"></div>
+        )}
+      </div>
 
       <div className="flex gap-3 w-full">
         {tags?.map((array, index) => (
@@ -19,6 +29,7 @@ const AmazingCard = ({ title, description, tags }) => {
           {description}
         </p>
       </div>
+      
     </div>
   );
 };
