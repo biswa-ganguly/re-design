5fa808594103381224df5b02ba8a953e7b395bea
biswa-ganguly <biswaganguly16@gmail.com>
2024-12-07 14:37:57 +0530

    animation

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,11 +6,13 @@ export default {
   ],
   theme: {
     extend: {
+
       fontFamily: {
         archivo: ['Archivo', 'sans-serif'],
         inter: ['Inter', 'sans-serif'],
         architects: ['Architects Daughter', 'sans-serif'],
       },
+
       keyframes: {
         stretchMove: {
           '0%': { transform: 'translateX(5%) scaleX(1)' },
@@ -21,22 +23,12 @@ export default {
           '0%, 100%': { transform: 'rotate(-10deg)' },
           '50%': { transform: 'rotate(10deg)' },
         },
-        slideInLeft: {
-          '0%': { transform: 'translateX(-100%)', opacity: 0 },
-          '100%': { transform: 'translateX(0)', opacity: 1 },
-        },
-        slideInRight: {
-          '0%': { transform: 'translateX(100%)', opacity: 0 },
-          '100%': { transform: 'translateX(0)', opacity: 1 },
-        },
       },
       animation: {
         stretchMove: 'stretchMove 1s ease-out forwards', 
         swing: 'swing 5s ease-in-out infinite', 
-        'slide-in-left': 'slideInLeft 0.7s ease-out',
-        'slide-in-right': 'slideInRight 0.7s ease-out',
       },
     },
   },
   plugins: [],
-};
+}
